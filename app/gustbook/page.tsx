0f978e5344af1c256c60db6213873c67b3fb93cb
GUNGODD <chauhanadityac01@gmail.com>
2024-05-05 01:01:28 +0530

    guestbook page

---
+++ app/gustbook/page.tsx
@@ -0,0 +1,9 @@
+export default function GuestbookPage() {
+  return (
+    <>
+      <section className=" max-w-7xl w-full px-4 md:px-8 mx-auto">
+        <h2> GuestBookPage</h2>
+      </section>
+    </>
+  );
+}
