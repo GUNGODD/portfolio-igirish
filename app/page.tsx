e48de2938703e6d00bdb181079236f390495ca39
GUNGODD <chauhanadityac01@gmail.com>
2024-05-06 15:39:44 +0530

    About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Hero } from "./Components/Hero";
 import { SectionTwo } from "./Components/SectionTwo";
+import { AboutSection } from "./Components/AboutSection";
 
 export default function Home() {
   return (
     <>
      <Hero/>
      <SectionTwo/>
+     <AboutSection/>
     </>
   );
 }
