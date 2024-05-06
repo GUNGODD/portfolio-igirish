d01973c74cb7467e922da2c88bd8b63ae7200c8a
GUNGODD <chauhanadityac01@gmail.com>
2024-05-06 15:32:46 +0530

    finished hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Hero } from "./Components/Hero";
+import { SectionTwo } from "./Components/SectionTwo";
 
 export default function Home() {
   return (
     <>
      <Hero/>
+     <SectionTwo/>
     </>
   );
 }
