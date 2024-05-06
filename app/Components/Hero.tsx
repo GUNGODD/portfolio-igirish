3cad0b94f6fe79c97978fcbf31f0c8ec1f248072
GUNGODD <chauhanadityac01@gmail.com>
2024-05-06 15:13:32 +0530

    hero section completed

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,28 +1,42 @@
+import Image from "next/image";
+import Me from "../../public/Me.jpg";
+
 export function Hero() {
   return (
-    <>
-      <div className=" grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <div className="col-span-1 lg:col-span-2 h-full bg-gray-100 min-h-[500px] lg:min-h-[300px] rounded-2xl p-8">
-          <h1 className="text-4xl md:text-6xl font-semibold ">
-            {" "}
-            Hey my name is Aditya 👻
-          </h1>
-          <h1 className="mt-6 text-xl md:text-2xl font-medium ">
-            {" "}
-            I am a Full Stack Web developer... 🦀
-          </h1>
-         <h1>
-         <a href="#_" className="relative inline-block text-lg group mt-8">
-<span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-<span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-<span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-<span className="relative">Get in Touch</span>
-</span>
-<span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
-</a>
-         </h1>
-        </div>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      <div className="col-span-1 lg:col-span-2 h-full bg-gray-100 min-h-[500px] lg:min-h-[300px] rounded-2xl p-8">
+        <h1 className="text-4xl lg:text-6xl font-medium">Hey I am Aditya 👻</h1>
+        <h1 className="text-4xl lg:text-6xl font-normal mt-10">
+          I am a{" "}
+          <span className="bg-transparent   text-8xl mb-4  ">
+             <span className="text-orange-500">W</span>eb  <span className="text-pink-500">D</span>
+             eveloper
+          </span>{" "}
+        <div className="m-4">  <span className="mt-10 p-4 gap-4">working and living in India 🦀</span></div>
+        </h1>
+
+        <a
+          href="mailto:ascenzen11@gmail.com"
+          className="relative inline-block text-lg group mt-6"
+        >
+          <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+            <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+            <span className="relative">Get in Touch!</span>
+          </span>
+          <span
+            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
+        </a>
       </div>
-    </>
+
+      <Image
+        src={Me}
+        alt="Aditya Singh"
+        className="col-span-1 h-[500px] object-cover rounded-2xl bg-gray-100"
+        priority
+      />
+    </div>
   );
 }
